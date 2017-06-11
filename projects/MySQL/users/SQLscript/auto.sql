6c6bb98901ad6e513046fa7207264d49b03a513a
yanterentev <yanterentev@gmail.com>
2017-06-11 13:34:26 +0300

    corrected

--- projects/MySQL/users/SQLscript/auto.sql
+++ projects/MySQL/users/SQLscript/auto.sql
@@ -1,18 +1,16 @@
-CREATE TABLE IF NOT EXISTS  autos(
-INT id NOT NULL PRIMARY KEY AUTO_INCREMENT,
-INT num NOT NULL,
-UNIQUE(num),
-color CHAR(10) NOT NULL,
-INT personnel_id NOT NULL
+CREATE TABLE IF NOT EXISTS autos(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	CHAR num(6) NOT NULL,
+	color CHAR(10) NOT NULL
+	INT personnel_id NOT NULL,
+	UNIQUE(num),
 FOREIGN KEY (personnel_id)
 REFERENCES auto_personnel(id)
 ON DELETE CASCADE
 );
 
-INSERT INTO autos(num, color, personnel_id) VALUES('175', 'white' 2);
-INSERT INTO autos(num, color, personnel_id) VALUES('269', 'blue' 3);
-INSERT INTO autos(num, color, personnel_id) VALUES('371', 'black' 2);
-INSERT INTO autos(num, color, personnel_id) VALUES('486', 'red' 6);
-INSERT INTO autos(num, color, personnel_id) VALUES('533', 'black' 5);
-
-SELECT * FROM autos ORDER BY field;
+INSERT INTO autos(num, color, personnel_id) VALUES('po175d', 'white' 2);
+INSERT INTO autos(num, color, personnel_id) VALUES('sd269w', 'blue' 3);
+INSERT INTO autos(num, color, personnel_id) VALUES('xc371r', 'black' 2);
+INSERT INTO autos(num, color, personnel_id) VALUES('kt465y', 'red' 6);
+INSERT INTO autos(num, color, personnel_id) VALUES('ot533o', 'black' 5);
--- projects/MySQL/users/SQLscript/auto_personnel.sql
+++ projects/MySQL/users/SQLscript/auto_personnel.sql
@@ -1,6 +1,13 @@
 CREATE TABLE IF NOT EXISTS  auto_personnels(
-INT id NOT NULL PRIMARY KEY AUTO_INCREMENT,
-first_name CHAR(10) NOT NULL,
-last_name CHAR(10) NOT NULL,
-pather_name CHAR(10) NOT NULL
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	first_name CHAR(10) NOT NULL,
+	last_name CHAR(10) NOT NULL,
+	pather_name CHAR(10) NOT NULL  
 );
+INSERT INTO auto_personnels(first_name, last_name, pather_name) VALUES('Ivan', 'Valerievich', 'Smirnov');
+INSERT INTO auto_personnels(first_name, last_name, pather_name) VALUES('Gleb', 'Nikitovich', 'Romanov');
+INSERT INTO auto_personnels(first_name, last_name, pather_name) VALUES('Roman', 'Valentinovich', 'Andreev');
+INSERT INTO auto_personnels(first_name, last_name, pather_name) VALUES('Miron', 'Maksimovich', 'Kazakov');
+INSERT INTO auto_personnels(first_name, last_name, pather_name) VALUES('Fedor', 'Andreevich', 'Gerasimov');
+
+SELECT last_name ORDER BY /* пока что незнаю как:(*/
