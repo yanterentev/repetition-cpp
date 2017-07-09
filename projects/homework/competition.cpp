84468496a93403d161fbba6e43c6e200dd8600b0
yanterentev <yanterentev@gmail.com>
2017-07-09 14:39:19 +0300

    competition

--- projects/homework/competition.cpp
+++ projects/homework/competition.cpp
@@ -1,51 +1,77 @@
 #include <iostream>
-using namespace std;
+#include <cstdlib>
+#include <cmath>
+#include <iomanip>
 
-int main()
+const int SPORTSMEN = 5;
+const int ATTEMPT = 3;
+
+double drand()
 {
-	int sportsmen = 5;
-	int attempt = 3;
-	double result[sportsmen][attempt];
+	double d1 = rand();
+	double d2 = rand();
+	double d3 = d2 / d1;
+	return d3 - floor(d3);
+}
 
-	std :: cout << "Введите результаты соревнований\n";
-	for(int i = 0; i < sportsmen; i++)
+void create(double result[SPORTSMEN][ATTEMPT])
+{
+	for(int i = 0; i < SPORTSMEN; i++)
 	{
-		std :: cout << "спортсмен " << (i + 1) << '\n';
-		for(int j = 0; j < attempt; j++)
+		for(int j = 0; j < ATTEMPT; j++)
 		{
-			std :: cin >> result[i][j];
+			result[i][j] = rand() % 10 + drand();
 		}
 	}
+}
 
-	double best_result = result[0][0];
-	int spmen;
-	int att;
-	for(int i = 0; i < sportsmen; i++)
+void print(double result[SPORTSMEN][ATTEMPT])
+{
+	for(int i = 0; i < SPORTSMEN; ++i)
 	{
-		for(int j = 0; j < attempt; j++)
+		for(int j = 0; j < ATTEMPT; ++j)
+		{
+			std :: cout << std :: setw(15) << result[i][j];
+		}
+		std :: cout << '\n';
+	}
+	std :: cout << '\n';
+}
+
+void find_best(double result[SPORTSMEN][ATTEMPT])
+{
+	int att;
+	double best_result = result[0][0];
+	double best_of_the_best = 0;
+	int best_sportsmen;
+	for(int i = 0; i < SPORTSMEN; i++)
+	{
+		for(int j = 0; j < ATTEMPT; j++)
 		{
 			if(best_result < result[i][j])
 			{
 				best_result = result[i][j];
-				spmen = i + 1;
 				att = j + 1;
 			}
 		}
-	}
-	std :: cout << '\n';
-	std :: cout << "лучший результат:\n";
-	for(int i = 0; i < sportsmen; i++)
-	{
-		for(int j = 0; j < attempt; j++)
+		if(best_result > best_of_the_best)
 		{
-			if (best_result == result[i][j])
-			{
-				std :: cout << "спортсмен  "<< (i + 1) << '\n'
-				<< "попытка s" << (j + 1) << '\n' << '\n';
-
-			}
+			best_of_the_best = best_result;
+			best_sportsmen = i;
 		}
+		std :: cout << "спортсмен " << i + 1 << " имеет лучший результат "
+		<< best_result << " достигнутый в попытке №" << att << '\n';
+		best_result = 0.0;
 	}
+	std :: cout << "самый лучший результат у спортсмена " << best_sportsmen + 1
+	<< " равный " << best_of_the_best << "!\n";
+}
+int main()
+{
+	double result[SPORTSMEN][ATTEMPT];
 
+	create(result);
+	print(result);
+	find_best(result);
 	return 0;
 }
